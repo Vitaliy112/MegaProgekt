90c1e3ba85bc69da8bbe5faa40b6a83276f26900
viktor <solonenko.vitaliy@mail.ru>
2016-12-21 22:16:29 +0200

    Cделал кнопку старт/стоп

--- MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
+++ MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
@@ -1,12 +1,18 @@
 package com.solonenko.vitaliy.megaprogekt123;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean isTrackengNoaw;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -23,7 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.app_name);
 
-
+         floatingActionButton = (FloatingActionButton) findViewById(R.id.btnStart);
+         floatingActionButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        isTrackengNoaw = !isTrackengNoaw;
+        floatingActionButton.setImageResource(isTrackengNoaw ? R.drawable.ic_stop_white_24dp : R.drawable.ic_play_arrow_white_24dp);
+        Toast.makeText(this, "Button presed", Toast.LENGTH_SHORT).show();
+
+    }
 }
