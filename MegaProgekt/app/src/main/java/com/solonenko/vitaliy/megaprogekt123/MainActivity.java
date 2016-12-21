ecc54c02ef6a8a325e84e4377e27379081ba8d9e
viktor <solonenko.vitaliy@mail.ru>
2016-12-21 21:36:04 +0200

    Сделал тул бар

--- MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
+++ MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
@@ -1,13 +1,29 @@
 package com.solonenko.vitaliy.megaprogekt123;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.app_name);
+
+
     }
+
 }
