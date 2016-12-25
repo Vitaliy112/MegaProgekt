ca67a47f210e2d4ea02d4f4992ae8dfb3660417b
viktor <solonenko.vitaliy@mail.ru>
2016-12-25 23:45:34 +0200

    Пермишины

--- MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
+++ MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
@@ -1,7 +1,12 @@
 package com.solonenko.vitaliy.megaprogekt123;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +15,10 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final int REQUEST_PERMISSION_ON_READ_FILE = 201;
+
 
     private boolean isTrackengNoaw;
     private FloatingActionButton floatingActionButton;
@@ -20,17 +29,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSION_ON_READ_FILE);
+        }
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(R.string.app_name);
 
-         floatingActionButton = (FloatingActionButton) findViewById(R.id.btnStart);
-         floatingActionButton.setOnClickListener(this);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.btnStart);
+        floatingActionButton.setOnClickListener(this);
     }
 
     @Override
@@ -39,5 +53,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         floatingActionButton.setImageResource(isTrackengNoaw ? R.drawable.ic_stop_white_24dp : R.drawable.ic_play_arrow_white_24dp);
         Toast.makeText(this, "Button presed", Toast.LENGTH_SHORT).show();
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_ON_READ_FILE) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Get read location permission", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
