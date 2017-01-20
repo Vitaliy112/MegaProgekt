f340d6e14d7774683401fafa66271bbf78c16e51
viktor <solonenko.vitaliy@mail.ru>
2017-01-20 21:13:17 +0200

    Добавил сервсы

---
+++ MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/Locacthinservisy.java
@@ -0,0 +1,76 @@
+package com.solonenko.vitaliy.megaprogekt123;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by vitaliy on 20.01.2017.
+ */
+
+public class Locacthinservisy  extends Service{
+
+    private LocationManager locationManager;
+
+    private static final String TAG = "Locacthinservisy";
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 1000 * 10, 1,
+                locationListener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000 * 10, 1,
+                locationListener);
+
+    }
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //   showLocation(location);
+            Log.d(TAG, "onLocationChanged() called with: location = [" + location + "]");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // checkEnabled();
+            Log.d(TAG, "onProviderDisabled() called with: provider = [" + provider + "]");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // checkEnabled();
+            // showLocation(locationManager.getLastKnownLocation(provider));
+            Log.d(TAG, "onProviderEnabled() called with: provider = [" + provider + "]");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                //  tvStatusGPS.setText("Status: " + String.valueOf(status));
+                //  } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                // tvStatusNet.setText("Status: " + String.valueOf(status));
+                Log.d(TAG, "onStatusChanged() called with: provider = [" + provider + "], status = [" + status + "], extras = [" + extras + "]");
+            }
+        }
+    };
+
+}
--- MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
+++ MegaProgekt/app/src/main/java/com/solonenko/vitaliy/megaprogekt123/MainActivity.java
@@ -1,10 +1,8 @@
 package com.solonenko.vitaliy.megaprogekt123;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +10,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -20,9 +17,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private LocationManager locationManager;
-    StringBuilder sbGPS = new StringBuilder();
-    StringBuilder sbNet = new StringBuilder();
+
     private static final String TAG = "MainActivity";
 
     private static final int REQUEST_PERMISSION_LOCATION = 201;
@@ -84,10 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void onPermissionResiv() {
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Toast.makeText(this, "Permission grandet", Toast.LENGTH_SHORT).show();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                1000 * 10, 10, locationListener);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -98,47 +90,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 1000 * 10, 1,
-                locationListener);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        Intent intent = new Intent(this, Locacthinservisy.class );
+         startService(intent);
 
     }
 
-    private LocationListener locationListener = new LocationListener() {
 
-        @Override
-        public void onLocationChanged(Location location) {
-            //   showLocation(location);
-            Log.d(TAG, "onLocationChanged() called with: location = [" + location + "]");
-        }
 
-        @Override
-        public void onProviderDisabled(String provider) {
-            // checkEnabled();
-            Log.d(TAG, "onProviderDisabled() called with: provider = [" + provider + "]");
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            // checkEnabled();
-            // showLocation(locationManager.getLastKnownLocation(provider));
-            Log.d(TAG, "onProviderEnabled() called with: provider = [" + provider + "]");
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            if (provider.equals(LocationManager.GPS_PROVIDER)) {
-                //  tvStatusGPS.setText("Status: " + String.valueOf(status));
-                //  } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
-                // tvStatusNet.setText("Status: " + String.valueOf(status));
-                Log.d(TAG, "onStatusChanged() called with: provider = [" + provider + "], status = [" + status + "], extras = [" + extras + "]");
-            }
-        }
-    };
 
 }
